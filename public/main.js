437830e8f26d6cecac6731ce07dd4b8e20de91a1
Jm1211 <110876203+Jm1211@users.noreply.github.com>
2022-12-28 14:49:07 -0700

    created modals for login and signup

---
+++ public/main.js
@@ -0,0 +1,26 @@
+
+
+
+const loginButton = document.querySelector('#login')
+const modalBg = document.querySelector('.modal-background')
+const modal = document.querySelector('.modal')
+
+const joinButton = document.querySelector('#signup')
+const joinModal = document.querySelector('#join')
+const joinBG = document.querySelector('#joinBg')
+
+loginButton.addEventListener('click', () => {
+    modal.classList.add('is-active');
+});
+
+modalBg.addEventListener('click', () => {
+    modal.classList.remove('is-active');
+});
+
+joinButton.addEventListener('click', () => {
+  joinModal.classList.add('is-active');
+});
+
+joinBG.addEventListener('click', () => {
+    joinModal.classList.remove('is-active');
+});
