7eb4f7293b98c293039a725a8b4a1209b3c9cc0d
Jm1211 <110876203+Jm1211@users.noreply.github.com>
2022-12-28 19:48:34 -0700

    updates

--- public/main.js
+++ public/main.js
@@ -24,3 +24,13 @@ joinButton.addEventListener('click', () => {
 joinBG.addEventListener('click', () => {
     joinModal.classList.remove('is-active');
 });
+
+
+(function () {
+    var burger = document.querySelector('.burger');
+    var menu = document.querySelector('#' + burger.dataset.target);
+    burger.addEventListener('click', function () {
+        burger.classList.toggle('is-active');
+        menu.classList.toggle('is-active');
+    });
+})();
