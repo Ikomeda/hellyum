3fb4bf0ae458602fb5b4f9e333dd657b875ec59d
Jm1211 <110876203+Jm1211@users.noreply.github.com>
2023-01-04 20:55:23 -0700

    start handlebars, finish look of site

--- CalenderPage/date.js
+++ CalenderPage/date.js
@@ -1,10 +1,2 @@
-function showDareInfo(){
-    let currentDate = new Date(); 
-    console.log(currentDate);
-    let startOfMonth = new Date(currentDate.getFullYear(), currentDate.getFullMonth(), 1);
-    console.log(startOfMonth);
-    const msToday = (24 * 60 * 60 * 1000);
-    const days = Math.floor((currentDate - startOfMonth)/ msToday);
-    console.log(days);
-    console.log(`week :${Math.cell(days/7)}`); 
-}
+var currentDate = moment().format("dddd, MMMM Do YYYY");
+$("#currentDay").text(currentDate);
