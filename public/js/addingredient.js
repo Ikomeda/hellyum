735e02475fa5f1d59a50921130ec8d48e71fc517
Jm1211 <110876203+Jm1211@users.noreply.github.com>
2023-01-08 22:36:04 -0700

    add create recipe form

---
+++ public/js/addingredient.js
@@ -0,0 +1,24 @@
+var ingredients = document.getElementById('ingredients');
+var add_ingredient = document.getElementById('add-ingredient');
+
+
+add_ingredient.onclick = function (){
+    var newIngredient = document.createElement('input');
+    newIngredient.setAttribute('type','text');
+    newIngredient.setAttribute('name', 'ingredients[]')
+    newIngredient.setAttribute('class', 'ingredients')
+    newIngredient.setAttribute('size',20);
+    newIngredient.setAttribute('placeholder', 'add ingredient')
+    ingredients.appendChild(newIngredient);
+
+    var newMeasurement = document.createElement('input');
+    newMeasurement.setAttribute('type','text');
+    newMeasurement.setAttribute('name', 'measurements[]')
+    newMeasurement.setAttribute('class', 'measurements')
+    newMeasurement.setAttribute('size',20);
+    newMeasurement.setAttribute('placeholder', 'add measurement')
+    ingredients.appendChild(newMeasurement);
+
+    
+}
+
